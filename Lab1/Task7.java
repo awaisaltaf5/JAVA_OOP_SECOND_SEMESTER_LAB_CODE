bfba2a774a7ae07ad17a841c5898aade87d330b0
Muhammad Awais Altaf <awaisgakhar28@gmail.com>
2024-09-18 20:36:02 +0500

    Test task 18/09/2024

---
+++ Lab1/Task7.java
@@ -0,0 +1,10 @@
+public class Task7 {
+    public static void main(String[] args) {
+        System.out.println("My name is Muhammad Awais Altaf"+"\nRoll No:Sp23948"+
+                "\nClass:2A");
+
+    }
+
+}
+
+
